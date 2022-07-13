848774aa1a841f08703f1730dc5cbc2cecf2ba8c
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-13 11:59:39 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -7,6 +7,16 @@ def checkoutRepository(gitCredentialsId1, url1) {
         }
 }
 
+def callAnotherJob(jobName) {
+   stage("Ovde ce biti definisani parametri i bice pozvan job2 bez parametara") {
+            steps {
+                script {
+                    build job: 'jobName', parameters: [[$class: 'StringParameterValue', name: 'Name', value: "Ana"]]
+                }
+            }
+        }
+}
+
 /*
 def checkoutRepository(gitCredentialsId1, url1) {
    stage('Git Checkout') {
