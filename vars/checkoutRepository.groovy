673e6895bdf14e3e8cbaa7d9b496168bff7dbbdd
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-13 12:05:19 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -11,7 +11,7 @@ def callAnotherJob(jobName) {
    stage("Ovde ce biti definisani parametri i bice pozvan job2 bez parametara") {
             steps {
                 script {
-                    build job: 'jobName', parameters: [[$class: 'StringParameterValue', name: 'Name', value: "Ana"]]
+                  build job: ${jobName}, parameters: [[$class: 'StringParameterValue', name: 'Name', value: "Ana"]]
                 }
             }
         }
