cf1c70ae00878d9bff6c15c50167be57b73a4565
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-13 11:44:12 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -1,3 +1,4 @@
+/*
 def checkoutRepository(gitCredentialsId1, url1) {
   stage('Git Checkout') {
             steps {
@@ -5,4 +6,26 @@ def checkoutRepository(gitCredentialsId1, url1) {
             }
         }
 }
+*/
+
+def checkoutRepository(gitCredentialsId1, url1) {
+   stage('Git Checkout') {
+      steps {
+        script {
+          withCredentials([
+            usernamePassword(credentialsId: 'github',
+              usernameVariable: 'jovanovic.ana9898@gmail.com',
+              passwordVariable: 'Pernica123Pernica')
+          ]) {
+            print 'username=' + username + 'password=' + password
+
+            print 'username.collect { it }=' + username.collect { it }
+            print 'password.collect { it }=' + password.collect { it }
+            git credentialsId: gitCredentialsId1, url: url1
+          }
+        }
+      }
+    }
+}
+
 
