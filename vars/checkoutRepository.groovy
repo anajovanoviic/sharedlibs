de64d58cea64a2d3ab64f17beb5949fe54dc05c9
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-13 14:14:18 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -27,7 +27,7 @@ def checkoutRepository1(gitCredentialsId1, url1) {
               usernameVariable: 'USERNAME',
               passwordVariable: 'PASSWORD')
           ]) {
-            git credentialsId: ${gitCredentialsId1}, url: url1
+            git credentialsId: ${gitCredentialsId1}, url: ${url1}
           }
         }
       }
