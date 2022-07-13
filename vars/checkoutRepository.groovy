96ddaa78ca322c4e99cd7b63c07f8ab7c3a75e0f
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-13 12:02:48 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -2,7 +2,7 @@
 def checkoutRepository(gitCredentialsId1, url1) {
   stage('Git Checkout') {
             steps {
-                git credentialsId: gitCredentialsId1, url: url1
+              git credentialsId: ${gitCredentialsId1}, url: ${url1}
             }
         }
 }
