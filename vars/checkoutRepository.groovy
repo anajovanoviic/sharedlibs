e2a23e64a781133141408ccf377ece58798aac57
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-14 10:08:52 +0200

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -44,7 +44,7 @@ def checkoutRepository2(repo, branchP) {
     ]) {
         // ovaj repo mora da bude prazan
         //sh "git clone https://$username:$password@github.com/anajovanoviic/didactic-system.git"
-        sh "git clone https://$username:$password@github.com/anajovanoviic/$repo.git --branch $branchP &> /dev/null" 
+        sh "git clone https://$username:$password@github.com/anajovanoviic/$repo.git --branch $branchP" 
     }
 }
 
