5c68230aa4396790752808765b7af368cda6e0f6
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-22 19:13:30 +0200

    Update ansibleMethod.groovy

--- vars/ansibleMethod.groovy
+++ vars/ansibleMethod.groovy
@@ -2,8 +2,8 @@ def call() {
 
  withCredentials([sshUserPrivateKey(credentialsId: 'private-key', keyFileVariable: 'ubuntu')]) {
   ansiColor('xterm') {
-                 sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i inventory/dev ping.yml -e 'ansible_ssh_private_key_file=$ubuntu ansible_user=anaworker ansible_sudo_pass=francuski1' " 
-                 //sh "\033[34m export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i inventory/dev ping.yml -e 'ansible_ssh_private_key_file=$ubuntu ansible_user=anaworker ansible_sudo_pass=francuski1' \033[0m" 
+                 //sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i inventory/dev ping.yml -e 'ansible_ssh_private_key_file=$ubuntu ansible_user=anaworker ansible_sudo_pass=francuski1' " 
+                 sh "\033[34m export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i inventory/dev ping.yml -e 'ansible_ssh_private_key_file=$ubuntu ansible_user=anaworker ansible_sudo_pass=francuski1' \033[0m" 
   }
   }
 
