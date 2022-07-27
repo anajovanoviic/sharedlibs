01c61b7a2d6034671d47a6ac41e3e2ff610fe084
anajovanoviic <51513732+anajovanoviic@users.noreply.github.com>
2022-07-27 14:07:32 +0200

    Create parsePayload.groovy

---
+++ vars/parsePayload.groovy
@@ -0,0 +1,24 @@
+import groovy.json.JsonSlurper
+
+
+def call() {
+   //probaj ovo
+   /**
+   println(currentBuild.getAbsoluteUrl())
+                    def url = "${currentBuild.getAbsoluteUrl()}api/json"
+                    println (url)
+   */
+   //ovo izmeni probaj ovako da parsisras payload webhooka
+  
+  String jsonURL = "http://localhost:8080/job/putanja/2/api/json?pretty=true"
+  
+  URL url = new URL(jsonURL)
+
+  def card = new JsonSlurper().parseText(url.text)
+
+  println(card.actions.causes.userName);
+  println(card.actions.causes.shortDescription);
+  println(card.fullDisplayName);
+  
+        
+}
